9f3bc2bb1f5becbae1c82f7f6318c92030313fcf
Andrii_Petrov <Andrii_Petrov@epam.com>
2023-02-27 16:24:16 +0100

    Set new name functionality

--- src/components/PlayerName.js
+++ src/components/PlayerName.js
@@ -5,10 +5,16 @@ import GameContext from "../store/game-context";
 
 const PlayerName = () => {
     const context = useContext(GameContext);
+
+    function setNameHandler() {
+        const playerName = prompt("Set new name:");
+        context.setPlayerName(playerName);
+    }
+
     return (
         <React.Fragment>
             {/*{context.playerName === "" && <NamePromptModal/>}*/}
-            <h2 className={classes.name}>Your name: {context.playerName}</h2>
+            <h2 className={classes.name} onClick={setNameHandler}>Your name: {context.playerName}</h2>
         </React.Fragment>
     );
 }
